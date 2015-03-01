52d9003ea89ffd3cee5f8e0eae869755b8d74b9b
Daniele Barsotti <daniele.barsotti@virtua.ch>
2015-03-01 14:50:44 +0100

    Hello Quil

--- hello-quil/src/hello_quil/core.clj
+++ hello-quil/src/hello_quil/core.clj
@@ -3,27 +3,22 @@
 
 
 (defn setup []
-  (q/smooth)                          ;; Turn on anti-aliasing
-  (q/frame-rate 10)                    ;; Set framerate to 1 FPS
-  (q/background 200))                 ;; Set the background colour to
-                                      ;; a nice shade of grey.
-(defn draw []
-  (q/stroke (q/random 255))             ;; Set the stroke colour to a random grey
-  (q/stroke-weight (q/random 10))       ;; Set the stroke thickness randomly
-  (q/fill (q/random 255))               ;; Set the fill colour to a random grey
+  (q/smooth)
+  (q/frame-rate 10)
+  (q/background 255)            
+  (q/fill 0)     
+  (q/text-font (q/create-font "DejaVu Sans" 48 true)))
 
-  (let [diam (q/random 100)             ;; Set the diameter to a value between 0 and 100
-        x    (q/random (q/width))       ;; Set the x coord randomly within the sketch
-        y    (q/random (q/height))]     ;; Set the y coord randomly within the sketch
-    (q/ellipse x y diam diam)))         ;; Draw a circle at x y with the correct diameter
+(defn draw []
+  (q/text "Hello Quil" (/ (q/width) 3) (/ (q/height) 2)))
 
 
 (defn foo []
 	(q/defsketch example                  ;; Define a new sketch named example
-	  :title "Oh so many grey circles"    ;; Set the title of the sketch
+	  :title "Hello Quil"    ;; Set the title of the sketch
 	  :setup setup                        ;; Specify the setup fn
 	  :draw draw                          ;; Specify the draw fn
-	  :size [800 600])   
+	  :size [800 600]) 
 )
 
 (defn -main [] (foo))
